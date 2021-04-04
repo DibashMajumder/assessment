5f324f009300cf0d3e0f40e416b2e8a12e59e640
Dibash99 <dayderrock@gmail.com>
2021-04-04 22:42:31 +0530

    Refactored code and optimized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import List from "./components/List";
 
 function App() {
+
+    const items = [
+        {
+            text: "This is text 1"
+        },
+        {
+            text: "This is text 2"
+        }
+    ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List items={items} />
     </div>
   );
 }
